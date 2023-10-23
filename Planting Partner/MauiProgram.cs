57e474f3c869811b74979034860ac0e82900d419
Axis Hax <yonathen.habte@gmail.com>
2023-10-22 21:25:39 -0400

    Initial Planting Partner project commit.

---
+++ Planting Partner/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Planting_Partner.Services;
+/*
+*
+*  This is the entry point for the app.
+*
+*/
+namespace Planting_Partner
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton<PlantService>();
+
+            builder.Services.AddSingleton<PlantsViewModel>();
+
+            builder.Services.AddSingleton<MainPage>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Planting Partner/Model/Plant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace Planting_Partner.Model
+{
+    public class Plant
+    {
+    }
+}
