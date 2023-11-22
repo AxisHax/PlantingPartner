b69e4af0d2bfe1bacb9a18dacdec24983b8b69b5
Axis Hax <yonathen.habte@gmail.com>
2023-11-21 21:14:38 -0500

    Added plant object and plant json file to project.

--- Planting Partner/Model/Plant.cs
+++ Planting Partner/Model/Plant.cs
@@ -9,5 +9,17 @@ namespace Planting_Partner.Model
 {
     public class Plant
     {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string GrowthZone { get; set; }
+        public string GrowSeason { get; set; }
+
+    }
+
+    [JsonSerializable(typeof(List<Plant>))]
+    internal sealed partial class PlantContext: JsonSerializerContext
+    {
+
     }
 }
